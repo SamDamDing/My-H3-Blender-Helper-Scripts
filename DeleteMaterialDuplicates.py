6f48cfcb6b6be070923c7d93a4d6a456e8a1f422
SamDamDing <45549722+SamDamDing@users.noreply.github.com>
2022-02-18 05:27:27 -0500

    Added a check to make sure that duplicate material had the same texture

--- DeleteMaterialDuplicates.py
+++ DeleteMaterialDuplicates.py
@@ -2,7 +2,17 @@ import bpy
 #Replaces any duplicate materials ending in .xxx with the original material, if available.
 mats = bpy.data.materials
 for obj in bpy.data.objects:
-    for slt in obj.material_slots:
-        part = slt.name.rpartition('.')
-        if part[2].isnumeric() and part[0] in mats:
-            slt.material = mats.get(part[0])
+    for s in obj.material_slots:
+        if s.material and s.material.use_nodes:
+            for n in s.material.node_tree.nodes:
+                if n.type == 'TEX_IMAGE':
+                    #texture_list += [n.image]
+                    imagename=n.image.name
+                    part = s.name.rpartition('.')
+                    if part[2].isnumeric() and part[0] in mats:
+                        ogmat=mats.get(part[0])
+                        for ogs in ogmat.node_tree.nodes:
+                            if ogs.type=='TEX_IMAGE':
+                                ogimg=ogs.image.name
+                                if ogimg==imagename:
+                                    s.material = ogmat
